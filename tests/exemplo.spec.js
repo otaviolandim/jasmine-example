4751a417e02a770f5cb08d5a7751aa2c0e380158
Otávio Landim <otavio.landim@gmail.com>
2016-03-29 01:00:07 -0300

    Removendo arquivos no diretorio antigo

--- tests/exemplo.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Iniciar o teste com 3d',function(){
-	it('Teste 01',function(){
-		var scene = new THREE.Scene();
-		expect(scene.fog).toBeNull();
-	})
-});
--- tests/soma.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Soma dois números', function(){
-  it('deve somar dois números positivos', function(){
-    expect(soma(1,3)).toEqual(4);
-  });
-
-  it('deve somar dois números iguais', function(){
-    expect(soma(1,1)).toEqual(2);
-  });
-});
